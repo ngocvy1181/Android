ad2f76946dc34bf315c9f9e9aabd3d410a8eda22
ngocvy1181 <ngocvy1181@outlook.com>
2017-05-18 09:39:46 +0700

    Bài tập 14- Thực hành về Custom Layout cho ListView trong Android

---
+++ Bai14_CustomLayoutvsListView/gen/com/example/bai14_customlayoutvslistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bai14_customlayoutvslistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
